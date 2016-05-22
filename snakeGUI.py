dc9c7b4ab4f5640d908fc5cec868f859e9553319
SamWu157 <cswu@cpp.edu>
2016-05-21 22:56:49 -0700

    Add files via upload

--- snakeGUI.py
+++ snakeGUI.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+import MySQLdb
 import random
 from Tkinter import *
 
@@ -12,6 +15,9 @@ class Game:
 
         # main window
         self.frame1 = None
+
+        # score window
+        self.frame2 = None
 
         # window
         self.w = None
@@ -52,11 +58,62 @@ class Game:
         # start button
         start = Button(self.frame1, text="start", bg="black", command=lambda: self.callgame(100))
 
+        # show score button
+        showScore = Button(self.frame1, text="Show Score", bg="black", command=lambda: self.showScore())
+
         start.grid(row=0, columnspan=2)
-        self.H=Label(self.root, text="CS 408\nJimmy Chen, Hung Lay, Samantha Wu", bg="black", fg="white", pady=10)
+        showScore.grid(row=1, columnspan=2)
+        self.H=Label(self.root, text="CS 408\nJimmy Cheng, Hung Lay, Samantha Wu", bg="black", fg="white", pady=10)
         self.H.pack()
         self.frame1.pack(expand=True)
         self.root.mainloop()
+
+    def showScore(self):
+        # close start window
+        self.frame1.destroy()
+
+        # draw frame 2
+        self.frame2 = Frame(self.root, width=750, height=350, bg="black")
+
+        # display Score
+        self.displayScore()
+
+        # draw back button
+        self.back = Button(self.frame2, text="Back", bg="black", command=lambda: self.backToStart())
+        self.back.pack(side="bottom")
+
+        # draw reset button
+        self.resetScore = Button(self.frame2, text="Reset", bg="black", command=lambda: self.reset())
+        self.resetScore.pack(side="bottom")
+
+        self.frame2.pack(expand=True)
+
+    # reset database
+    def reset(self):
+
+        # reset database
+        resetDB()
+
+        # close score window
+        self.frame2.destroy()
+
+        # close header
+        self.H.destroy()
+
+        # call start window
+        self.home()
+
+    # back button
+    def backToStart(self):
+
+        # close score window
+        self.frame2.destroy()
+
+        # close header
+        self.H.destroy()
+
+        # call start window
+        self.home()
 
     # start game    
     def callgame(self, time):
@@ -207,8 +264,7 @@ class Game:
                 self.w.delete(self.head)
                 self.w.delete(self.food)
                 self.restart = Button(self.root, text="Restart", command=lambda: self.callhome())
-                self.restart.pack(side="bottom", side="left")
-
+                self.restart.pack(side="bottom")
 
     # down arrow
     def down(self, i):
@@ -287,11 +343,190 @@ class Game:
         return False
 
     def callhome(self):
+
         self.w.destroy()
         self.restart.destroy()
         self.H.destroy()
         self.scoreC.destroy()
+
+        # ask for name
+        self.input = Label(self.root, text="Name (3 CHAR ONLY)", bg = "black", fg = "white")
+        self.input.pack( side = LEFT)
+        self.entry = Entry(self.root, bd =5)
+        self.entry.pack(side = LEFT)
+        self.submit = Button(self.root, text="submit", bg="black", command=lambda: self.store(self.entry.get().strip()))
+        self.submit.pack(side = RIGHT)
+
+    def store(self, name):
+        DB(name, self.score)
+
+        self.input.destroy()
+        self.entry.destroy()
+        self.submit.destroy()
+
         self.home()
+
+    def displayScore(self):
+
+        # set up database
+        self.DB_HOST = "localhost"
+        self.DB_USER = "cs408_user"
+        self.DB_PW = "cs408"
+        self.DB_DB = "game"
+
+        # Open databse connection
+        self.db = MySQLdb.connect(self.DB_HOST, self.DB_USER, self.DB_PW, self.DB_DB)
+
+        # prepare cursor object
+        self.cursor = self.db.cursor()
+
+        # get ranks
+        sql = "SELECT rank FROM score"
+        self.cursor.execute(sql)
+        rank = self.cursor.fetchall()
+        
+        rankList = []
+        for i in rank:
+            rankList.append(i[0])
+
+        rankList = sorted(rankList)
+
+        # print
+        if not rankList:
+            label = Label(self.frame2, text="No Scores Available", bg="black", fg="white") 
+            label.pack()
+        else:
+            header = Label(self.frame2, text="%-10s%-10s%-10s\n" % ("RANK", "NAME", "SCORE"), bg="black", fg="white")
+            header.pack()
+
+            for i in range(0, len(rankList)):
+
+                sql = "SELECT name FROM score WHERE rank = '%d'" % (i + 1)
+                self.cursor.execute(sql)
+                name = self.cursor.fetchall()
+                sql = "SELECT score FROM score WHERE rank = '%d'" % (i + 1)
+                self.cursor.execute(sql)
+                score = self.cursor.fetchall()
+
+                display = Label(self.frame2, text="%-10d%-10s%-10d" % (i + 1, name[0][0], score[0][0]), bg="black",fg="white")
+                display.pack()
+ 
+
+        # close database
+        self.db.close()
+
+# set up database
+class DB:
+
+    def __init__(self, name, score):
+        # set up values
+        self.DB_HOST = "localhost"
+        self.DB_USER = "cs408_user"
+        self.DB_PW = "cs408"
+        self.DB_DB = "game"
+
+        # Open databse connection
+        self.db = MySQLdb.connect(self.DB_HOST, self.DB_USER, self.DB_PW, self.DB_DB)
+
+        # prepare cursor object
+        self.cursor = self.db.cursor()
+
+        # store score
+        self.name = name
+        self.score = score
+        self.addScore()
+
+        # commit to database
+        self.db.commit()
+
+        # disconnect from server
+        self.db.close()
+
+    def sort(self):
+
+        # get ids
+        sql = "SELECT id FROM score"
+        self.cursor.execute(sql)
+        ids = self.cursor.fetchall()
+
+        # convert to values
+        idList = []
+        for i in ids:
+            idList.append(i[0])
+
+        # get scores
+        sql = "SELECT score FROM score"
+        self.cursor.execute(sql)
+        score = self.cursor.fetchall()
+
+        # convert to values
+        scoreList = []
+        for i in score:
+            scoreList.append(i[0])
+
+        # map score to id
+        rankingScore = zip(idList, scoreList)
+
+        ranking = sorted(rankingScore, key=lambda rankingScore: rankingScore[1])
+
+        print ranking
+
+        reverse = len(rankingScore) - 1
+
+        for i in range(0, len(ranking)):
+
+            print ranking[reverse][1]
+
+            sql = "UPDATE score SET rank = '%d' WHERE id = '%d'" % (i + 1, ranking[reverse][0])
+            self.cursor.execute(sql)
+
+            reverse -= 1;
+
+        # commit to database
+        self.db.commit()
+
+    def addScore(self):
+        # store new score
+        sql = "INSERT INTO score (rank, name, score) VALUES (0, '%s', '%d')" % (self.name, self.score)
+        self.cursor.execute(sql)
+
+        # sort database
+        self.sort()
+
+class resetDB:
+    def __init__(self):
+        # set up values
+        self.DB_HOST = "localhost"
+        self.DB_USER = "cs408_user"
+        self.DB_PW = "cs408"
+        self.DB_DB = "game"
+
+        # Open databse connection
+        self.db = MySQLdb.connect(self.DB_HOST, self.DB_USER, self.DB_PW, self.DB_DB)
+
+        # prepare cursor object
+        self.cursor = self.db.cursor()
+
+        # resetScore
+        self.resetScore()
+
+        # commit to database
+        self.db.commit()
+
+        # disconnect from server
+        self.db.close()
+
+    def resetScore(self):
+
+        # delete table
+        self.cursor.execute("DROP TABLE IF EXISTS score")
+
+        # add empty table
+        sql = "CREATE TABLE score (rank int, name VARCHAR(3), score int, id int NOT NULL AUTO_INCREMENT PRIMARY KEY)"
+        self.cursor.execute(sql)
+
+        # commit to database
+        self.db.commit()
 
 g = Game()
 g.home()
